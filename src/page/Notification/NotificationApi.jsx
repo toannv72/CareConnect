dddbdbc6fb4a00efacb46a4499d8e93b9284411e
nguyen van toan <64313624+toannv72@users.noreply.github.com>
2024-07-11 03:19:12 +0700

    up

--- src/page/Notification/NotificationApi.jsx
+++ src/page/Notification/NotificationApi.jsx
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Alert } from "react-native";
 import messaging from "@react-native-firebase/messaging";
 import React, { useEffect } from "react";
+
 export default function NotificationApi() {
   const requestUserPermission = async () => {
     const authStatus = await messaging().requestPermission();
@@ -13,47 +14,73 @@ export default function NotificationApi() {
       console.log("Authorization status:", authStatus);
     }
   };
-  
 
   useEffect(() => {
-    if (requestUserPermission()) {
+    const handleNotificationSetup = async () => {
+      await requestUserPermission();
+
       messaging()
         .getToken()
         .then((token) => {
           console.log(token);
+        })
+        .catch((error) => {
+          console.log("Error getting token:", error);
         });
-    } else {
-      console.log("Failed token status");
-    }
-    messaging()
-      .getInitialNotification()
-      .then(async (remoteMessage) => {
-        if (remoteMessage) {
-          console.log(
-            "Notification caused app to open from quit state:",
-            remoteMessage.notification
-          );
-        }
+
+      messaging()
+        .getInitialNotification()
+        .then((remoteMessage) => {
+          if (remoteMessage) {
+            console.log(
+              "Notification caused app to open from quit state:",
+              remoteMessage.notification
+            );
+          }
+        });
+
+      messaging().onNotificationOpenedApp((remoteMessage) => {
+        console.log(
+          "Notification caused app to open from background state:",
+          remoteMessage.notification
+        );
       });
-    messaging().onNotificationOpenedApp((remoteMessage) => {
-      console.log(
-        "Notification caused app to open from background state:",
-        remoteMessage.notification
-      );
-    });
-    // Register background handler Đăng ký xử lý nền
-    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-      console.log("Message handled in the background!", remoteMessage);
-    });
-    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
-      Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
-    });
-    return unsubscribe;
+
+      messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+        console.log("Message handled in the background!", remoteMessage);
+      });
+
+      const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+        Alert.alert(
+          "A new FCM message arrived!",
+          JSON.stringify(remoteMessage)
+        );
+      });
+
+      return unsubscribe;
+    };
+
+    const unsubscribe = handleNotificationSetup();
+
+    return () => {
+      if (typeof unsubscribe === "function") {
+        unsubscribe();
+      }
+    };
   }, []);
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>FCM Tutorial</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
