f9aecce06675bb07b26450c7adba7d557189e719
thaomy <113156865+thaomy200602@users.noreply.github.com>
2024-08-07 02:57:47 +0700

    Service home filter

--- src/page/Home/News/Services.jsx
+++ src/page/Home/News/Services.jsx
@@ -25,7 +25,8 @@ export default function Services() {
     if (selectedCategory) { url += `&PackageCategoryId=${selectedCategory}` }
     getData(url, {})
       .then((packageData) => {
-        setData(packageData?.data?.contends);
+        const filterData = packageData?.data?.contends?.filter(item => item.state === true);
+        setData(filterData);
         setLoading(loading);
       })
       .catch((error) => {
@@ -58,7 +59,7 @@ export default function Services() {
     const hasNotExpired = service?.type == "OneDay" ? moment().isSameOrBefore(endRegistrationDate, "day") : true;
     const hasSlotsLeft = service?.type == "OneDay" ? service?.registrationLimit !== 0 ? service?.totalOrder < service?.registrationLimit : service?.totalOrder >= service?.registrationLimit : true;
     return hasNotExpired && hasSlotsLeft;
-});
+  });
 
   return (
     <View style={styles?.body}>
